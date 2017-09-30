e6d51216af3cdba128a53466e87408a9f310f325
Jose Angel <esojangel@gmail.com>
2017-09-30 17:35:06 -0400

    Create only_read.sql

---
+++ only_read.sql
@@ -0,0 +1,14 @@
+SELECT c FROM sbtest WHERE id = trunc((random() * (1000000 - 1)) + 1);
+SELECT c FROM sbtest WHERE id = trunc((random() * (1000000 - 1)) + 1);
+SELECT c FROM sbtest WHERE id = trunc((random() * (1000000 - 1)) + 1);
+SELECT c FROM sbtest WHERE id = trunc((random() * (1000000 - 1)) + 1);
+SELECT c FROM sbtest WHERE id = trunc((random() * (1000000 - 1)) + 1);
+SELECT c FROM sbtest WHERE id = trunc((random() * (1000000 - 1)) + 1);
+SELECT c FROM sbtest WHERE id = trunc((random() * (1000000 - 1)) + 1);
+SELECT c FROM sbtest WHERE id = trunc((random() * (1000000 - 1)) + 1);
+SELECT c FROM sbtest WHERE id = trunc((random() * (1000000 - 1)) + 1);
+SELECT c FROM sbtest WHERE id = trunc((random() * (1000000 - 1)) + 1);
+SELECT c FROM sbtest WHERE id BETWEEN trunc((random() * (1000000 - 1)) + 1) AND trunc((random() * (1000000 - 1)) + 1) + 100;
+SELECT SUM(K) FROM sbtest WHERE id BETWEEN trunc((random() * (1000000 - 1)) + 1) AND trunc((random() * (1000000 - 1)) + 1) + 100;
+SELECT c FROM sbtest WHERE id BETWEEN trunc((random() * (1000000 - 1)) + 1) AND trunc((random() * (1000000 - 1)) + 1) + 100 ORDER BY c;
+SELECT DISTINCT c FROM sbtest WHERE id BETWEEN trunc((random() * (1000000 - 1)) + 1) AND trunc((random() * (1000000 - 1)) + 1) + 100;
